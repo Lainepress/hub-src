6942f2e504f85e8c2df2ee4eadba6108d7dc049e
Chris Dornan <chris@chrisdornan.com>
2011-10-17 18:44:03 +0100

    tidy source tree

--- x/pastebin.hs
+++
@@ -1,7 +0,0 @@
-trim :: String -> String
-trim ln0 =
-        case reverse ln of
-          c:r_cs | isSpace c -> reverse $ dropWhile isSpace r_cs
-          _                  -> ln
-      where
-        ln = dropWhile isSpace ln0
